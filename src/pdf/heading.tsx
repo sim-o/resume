1fe6e008b2fdde00230705a4d50b883cfa430385
Simon Kerle <sim-o@users.noreply.github.com>
2024-06-21 11:50:04 +1000

    styling

---
+++ src/pdf/heading.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text, View } from '@react-pdf/renderer';
+import { styles } from '../styles';
+
+type Props = {children: React.ReactNode, underline?: boolean, style?: any};
+export function Heading({children, style, underline = true}: Props) {
+  return (
+    <View style={[styles.columnTitle, style]}>
+      <Text>{children}</Text>
+      {underline && <View style={styles.columnTitleUnderline}/>}
+    </View>
+  );
+}
