760b2a24a68c491dd755de95a8c378d6c29ac2f3
Simon Kerle <sim-o@users.noreply.github.com>
2024-07-05 14:18:33 +1000

    move special evaluation names to constants

--- src/evaluateConditionals.tsx
+++ src/evaluateConditionals.tsx
@@ -6,6 +6,9 @@ function evaluate(when: string): boolean {
   return !!new Function('env', `return (${when});`)(process.env);
 }
 
+const WHEN_TOKEN = '$when';
+const VALUE_TOKEN = '$value';
+
 /**
  * Finds objects with a `$when` property and evaluates them. If true, returns the object excluding the `$when` property,
  * otherwise the object is removed from the tree. If there is a `$value` property it is returned instead of the
@@ -23,20 +26,20 @@ export function evaluateConditionals<T>(o: T): T | undefined {
     return cleaned.length ? (cleaned as T) : undefined;
   }
 
-  if ('$when' in o) {
-    if (typeof o.$when === 'string') {
-      const when = evaluate(o.$when);
+  if (WHEN_TOKEN in o) {
+    if (typeof o[WHEN_TOKEN] === 'string') {
+      const when = evaluate(o[WHEN_TOKEN]);
       if (!when) return undefined;
-    } else if (!o.$when) return undefined;
+    } else if (!o[WHEN_TOKEN]) return undefined;
   }
 
-  if ('$value' in o) {
-    return evaluateConditionals(o.$value) as T;
+  if (VALUE_TOKEN in o) {
+    return evaluateConditionals(o[VALUE_TOKEN]) as T;
   }
 
   return Object.fromEntries(
     Object.entries(o)
-      .filter(([k]) => k !== '$when')
+      .filter(([k]) => k !== WHEN_TOKEN)
       .map(([k, v]) => [k, evaluateConditionals(v)])
       .filter(([, v]) => v !== undefined)
   ) as T;
